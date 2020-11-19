a42740f45805940c5f9de8098738659dde65360f
saddysphere <jorgenwhen@gmail.com>
2020-11-19 10:45:19 +0100

    implementation

--- cartpole/dqn/agent.py
+++ cartpole/dqn/agent.py
@@ -1,23 +1,35 @@
 from qnetwork import QNetwork
-from memory import Memory
 import numpy as np
 import torch
 
 class Agent:
     def __init__(self, gamma, epsilon, lr, in_features, batch_size, n_actions, 
-            mem_size=100_000, ep_min=0.01, ep_decay=5e-4):
+            mem_size=100_000, ep_min=0.01, ep_decay=5e-4, cuda=True):
         self.gamma = gamma
         self.epsilon = epsilon
         self.ep_min = ep_min
         self.ep_decay = ep_decay
         self.action_space = [i for i in range(n_actions)]
         self.batch_size = batch_size
-        self.memory = Memory(mem_size)
+        self.mem_size = mem_size
+        self.mem_cntr = 0
 
-        self.q_network = QNetwork(lr, in_features, 256, 256, n_actions)
+        self.state_memory = np.empty((self.mem_size, in_features))
+        self.action_memory = np.empty(self.mem_size, dtype=np.int32)
+        self.reward_memory = np.empty(self.mem_size)
+        self.next_state_memory = np.empty((self.mem_size, in_features))
+        self.done_memory = np.empty(self.mem_size, dtype=np.bool)
+
+        self.q_network = QNetwork(lr, in_features, 256, 256, n_actions, cuda)
 
     def remember(self, state, action, reward, next_state, done):
-        self.memory.add_memory(state, action, reward, next_state, done)
+        index = self.mem_cntr % self.mem_size
+        self.state_memory[index] = state
+        self.action_memory[index] = action
+        self.reward_memory[index] = reward
+        self.next_state_memory[index] = next_state
+        self.done_memory[index] = done
+        self.mem_cntr += 1
 
     def act(self, state):
         if np.random.random() < self.epsilon:
@@ -28,27 +40,29 @@ class Agent:
         return torch.argmax(actions).item()
 
     def learn(self):
-        if self.memory.size() < self.batch_size:
+        if self.mem_cntr < self.batch_size:
             return
         
         self.q_network.train()
         self.q_network.optimizer.zero_grad()
 
-        states, actions, rewards, next_states, dones = self.memory.get_batch(self.batch_size)
+        max_mem = min(self.mem_cntr, self.mem_size)
+        batch = np.random.choice(max_mem, self.batch_size, replace=False)
+        batch_index = np.arange(self.batch_size)
 
-        state_batch = torch.Tensor(states).to(self.q_network.device)
-        action_batch = torch.Tensor(actions).type(torch.long)
-        reward_batch = torch.Tensor(rewards).to(self.q_network.device)
-        next_state_batch = torch.Tensor(next_states).to(self.q_network.device)
-        done_batch = torch.Tensor(dones).type(torch.bool).to(self.q_network.device)
+        state_batch = torch.Tensor(self.state_memory[batch]).to(self.q_network.device)
+        action_batch = self.action_memory[batch]
+        reward_batch = torch.Tensor(self.reward_memory[batch]).to(self.q_network.device)
+        next_state_batch = torch.Tensor(self.next_state_memory[batch]).to(self.q_network.device)
+        done_batch = torch.Tensor(self.done_memory[batch]).type(torch.BoolTensor).to(self.q_network.device)
 
-        q_next = self.q_network(next_state_batch)
-        q_targets = reward_batch + self.gamma * torch.max(q_next, dim=1)[0]
-        q_next[done_batch] = 0.0
-        q_next[action_batch.reshape(self.batch_size, 1)] = q_targets
-        q_eval = self.q_network(state_batch)
+        q_eval = self.q_network(state_batch)[batch_index, action_batch]
+        q_eval_next = self.q_network(next_state_batch)
+        q_eval_next[done_batch] = 0.0
 
-        loss = self.q_network.loss_function(q_targets, q_eval).to(self.q_network.device)
+        q_target = reward_batch + self.gamma * torch.max(q_eval_next, dim=1)[0]
+
+        loss = self.q_network.loss_function(q_target, q_eval).to(self.q_network.device)
         loss.backward()
         self.q_network.optimizer.step()
 
--- cartpole/dqn/main.py
+++ cartpole/dqn/main.py
@@ -1,14 +1,26 @@
 import gym
-from agent import Agent
+import argparse
 import numpy as np
+import matplotlib.pyplot as plt
+from agent import Agent
 
-if __name__ == "__main__":
+import os
+os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 
+def plot_results(episodes, scores, filename):
+    plt.plot(episodes, scores)
+    plt.xlabel("episodes")
+    plt.ylabel("scores")
+    plt.savefig(filename)
+
+def cartpole(args):
     env = gym.make('CartPole-v1')
-    agent = Agent(gamma=0.99, epsilon=1.0, lr=0.001, in_features=4, batch_size=64, 
-                n_actions=2, mem_size=100_000, ep_min=0.01, ep_decay=5e-4) 
+    agent = Agent(gamma=args.gamma, epsilon=args.epsilon, lr=args.learnrate, in_features=4, 
+                batch_size=args.batch, n_actions=2, mem_size=args.memsize, ep_min=0.01, 
+                ep_decay=args.decay, cuda=args.cuda) 
 
-    n_games = 10_000
+    render = args.render
+    n_games = args.maxgames
     episodes = 0
     scores = []
     solved = False
@@ -19,7 +31,9 @@ if __name__ == "__main__":
         observation = env.reset()
         
         while not done:
-            env.render()
+            if render:
+                env.render()
+
             action = agent.act(observation)
             observation_, reward, done, info = env.step(action)
             score += reward
@@ -27,14 +41,34 @@ if __name__ == "__main__":
             agent.remember(observation, action, reward, observation_, done)
             agent.learn()
 
-            if done:
-                episodes += 1
-                scores.append(score)
+            observation = observation_
 
-                mean_score = np.mean(scores[-100:])
-                print(round(mean_score, 1))
-                if mean_score >= 195.0:
-                    solved = True
-                    print("SOLVED")
+        episodes += 1
+        scores.append(score)
+        solved = min(scores[-100:]) >= 195.0
+
+        print(f"episode: {episodes} - highest score: {max(scores)}    \r", end="")
+    
+    if args.file:
+        plot_results(episodes, scores, args.file)
 
     env.close()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Deep reinforcement learning for the CartPole-v1 environment from openai-gym.")
+
+    parser.add_argument('-r', '--render', help="Render the game screen.", action='store_true')
+    parser.add_argument('-f', '--file', help="The output filename for the plots. No plot will be created if unspecified.", type=str, default=None)
+    parser.add_argument('-ep', '--epsilon', help="The starting epsilon value for the playing agent.", type=float, default=1.0)
+    parser.add_argument('-de', '--decay', help="The epsilon decay factor.", type=float, default=5e-4)
+    parser.add_argument('-ga', '--gamma', help="The gamma (discount) factor.", type=float, default=0.99)
+    parser.add_argument('-lr', '--learnrate', help="The learning rate for the neural network.", type=float, default=0.001)
+    parser.add_argument('-b', '--batch', help="The batch size used during learning.", type=int, default=64)
+    parser.add_argument('-mem', '--memsize', help="The agent memory size.", type=int, default=100_000)
+    parser.add_argument('-cu', '--cuda', help="Whether to use cuda for the neural network.", action="store_true")
+    parser.add_argument('-max', '--maxgames', help="The maximum amount of games played before the training terminates.", type=int, default=np.inf)
+
+    args = parser.parse_args()
+    cartpole(args)
+
+    
